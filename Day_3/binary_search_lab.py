e7d7a8165cb6bf1b2fa88590bd765b5389524f45
Bob Mwaniki <Bob Mwaniki>
2016-10-11 18:54:01 +0300

    Made Binary Search cleaner

--- Day_3/binary_search_lab.py
+++ Day_3/binary_search_lab.py
@@ -3,12 +3,25 @@ class BinarySearch(list):
 	def __init__(self, a, b):
 		self.length = a
 		self.step = b
+		for i in range(self.step, (self.length*self.step)+1, self.step):
+			self.append(i) 
 
 	def search(self, value):
-		index = 0
-		for item in self:
-			if value == item:
-				break
+		first_index = 0
+		last_index = self.length - 1
+		count = 0
+		index = -1
+		found_value = False
+		while first_index <= last_index and not found_value:
+			midpoint = (first_index + last_index)//2
+			if self[midpoint] == value:
+				count += 1
+				index = midpoint
+				found_value = True
 			else:
-				index += 1
-		return {'count':index}
+				count += 1
+				if value < self[midpoint]:
+					last_index = midpoint - 1
+				else:
+					first_index = midpoint + 1
+		return {'count': count, 'index': index}

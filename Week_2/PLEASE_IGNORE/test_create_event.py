1fb8dd44d6275f77e17a5349a490a090c83493c5
swedge <scott.wedge@gmail.com>
2020-02-12 13:30:16 -0500

    Create first test that always passes

--- Week_2/PLEASE_IGNORE/test_create_event.py
+++ Week_2/PLEASE_IGNORE/test_create_event.py
@@ -1,12 +1,15 @@
 import unittest, sqlite3
-from create event import create_event
+from create_event import create_event
 
 conn = sqlite3.connect('dummy_data.db')
 c = conn.cursor()
 
+class TestInitialClass(unittest.TestCase):
+    def test_pass(self):
+         self.assertEqual(True,True)
 
 
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	unittest.main()
